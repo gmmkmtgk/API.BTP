f785dd2f9be87316aeacd94b7c58335be2ffb1c8
Keshav Maheshwari <keshavmaheshwari800@gmail.com>
2022-08-16 20:17:45 +0530

    step 1

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // Spend a minute memorizing the line below
 // Re-write the line of code as best you can from memory
-ReactDOM.render(<p>Hi, my name is Bob!</p>, document.getElementById("root"))
-
+//ReactDOM.render(<p>Hi, my name is Bob!</p>, document.getElementById("root"))
+ReactDOM.render(
+    <ul><li>Thing 1</li><li>Thing 2</li></ul>,
+    document.getElementById("root")
+)
 
 
 
