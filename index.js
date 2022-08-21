57126189eed2334eddae29297a66b3aa7707ac14
Keshav Maheshwari <keshavmaheshwari800@gmail.com>
2022-08-21 11:43:49 +0530

    adding compontes and files

--- index.js
+++ index.js
@@ -1,38 +1,17 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import Header from "./Header"
+import Footer from "./Footer"
+import MainContent from "./MainContent"
 
-/**
-Mini Challenge:
-Move the `header` element from Page into 
-its own component called "Header"
-*/
-
-function Header() {
-    return (
-        <header>
-            <nav>
-                <img src="./react-logo.png" width="40px" />
-            </nav>
-        </header>
-    )
-}
-
-function Page() {
+function App() {
     return (
         <div>
             <Header />
-            <h1>Reasons I'm excited to learn React</h1>
-            <ol>
-                <li>It's a popular library, so I'll be 
-                able to fit in with the cool kids!</li>
-                <li>I'm more likely to get a job as a developer
-                if I know React</li>
-            </ol>
-            <footer>
-                <small>© 2021 Ziroll development. All rights reserved.</small>
-            </footer>
+            <MainContent />
+            <Footer />
         </div>
     )
 }
 
-ReactDOM.render(<Page />, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"))
