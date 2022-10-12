aa8edeaf6e895eb4db67200f222a8ccf024b30c2
Keshav Maheshwari <keshavmaheshwari800@gmail.com>
2022-10-12 17:37:45 +0530

    Added Req and JOb schedule

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ from Controllers import gentable, genresult, genratecsvfiles
 from pathlib import Path
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+from fastapi_utils.session import FastAPISessionMaker
+from fastapi_utils.tasks import repeat_every
 
 app = FastAPI()
+
+@app.get("/")
+def refreshprices():
+    return "Welcome to Momentum Investing Portal"
+
 
 @app.get("/getstocks/{week}/{month}/{tmonth}/{smonth}/{year}")
 def getstocks(week: int, month: int, tmonth: int, smonth: int, year: int):
@@ -18,14 +25,14 @@ def getstocks(week: int, month: int, tmonth: int, smonth: int, year: int):
         resultStocks.to_csv(filename)
     return FileResponse(filename)
 
-
-@app.post("/refreshprices")
-def refreshprices():
+@app.on_event("startup")
+@repeat_every(seconds=60*60*6)  # 6 hour
+def refreshprices() -> None:
     try:
         for filename in glob.glob("Result*"):
             os.chmod(filename, 0o777)
             os.remove(filename)
         genratecsvfiles.genratezips()
-        return {"message" : "Sucessfully done"}
     except Exception as e:
-        return {"error":str(e)}
+        pass
+
